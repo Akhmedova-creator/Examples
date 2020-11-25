434978964529030ea11e5d05dd2b3710c2127844
sdddd <fazik_agent007@mail.ru>
2020-11-25 16:22:59 +0500

    Регистрация config-server в eureka

--- camunda_eureka/mcg-configuration-service/src/main/java/org/mcg/configuration/service/ConfigServiceApplication.java
+++ camunda_eureka/mcg-configuration-service/src/main/java/org/mcg/configuration/service/ConfigServiceApplication.java
@@ -4,11 +4,12 @@ package org.mcg.configuration.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
 @SpringBootApplication
-@EnableFeignClients
+@EnableEurekaClient
+
 public class ConfigServiceApplication {
     
     public static void main(String[] args) {
